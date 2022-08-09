78153009d66ed694d5555ca64b3ffcaaf82e99f5
jancy <jotabbusiness@icloud.com>
2022-08-08 22:55:33 -0500

    api for users

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from './robots'
 import SearchBox from "./SearchBox";
 import './App.css'
+import Scroll from "./Scroll";
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(user => this.setState({ robots: user }))
+
+    }
+
 
     onSearchChange = (e) => {
         this.setState({ searchfield: e.target.value })
     }
 
-     render() {
+    render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        })
-        return (
-            <div className='tc' >
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        });
+
+        if (this.state.robots.length === 0) {
+            return (
+                <div>
+                    <h1 className='loader ma7'></h1>
+                </div>
+            )
+        } else {
+            return (
+                <div className='tc' >
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
--- src/Card.js
+++ src/Card.js
@@ -8,10 +8,10 @@ const Card = ({id, name, email}) => {
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='Robot' src={`https://robohash.org/${id}?100x100`} />
             <div>
-                <h2>name: {name}</h2>
+                <h2>{name}</h2>
             </div>
             <div>
-                <p>email: {email}</p>
+                <p>{email}</p>
             </div>
         </div>
     );
